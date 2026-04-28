90131a2c919055447f5002eacca9b59af7b870e6
Leoz168 <168997688+Leoz168@users.noreply.github.com>
2026-04-27 22:22:36 -0400

    fix the JWT

--- apps/api/cmd/api/main.go
+++ apps/api/cmd/api/main.go
@@ -45,6 +45,12 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(response)
 }
 
+func authRequired(handler http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		middleware.Authenticate(handler).ServeHTTP(w, r)
+	}
+}
+
 func main() {
 	appConfig := config.Load()
 	var err error
@@ -122,41 +128,41 @@ func main() {
 	http.HandleFunc("/api/health", healthHandler)
 
 	// User endpoints
-	http.HandleFunc("/api/users", user.GetUsers)
-	http.HandleFunc("/api/users/get", user.GetUser)
-	http.HandleFunc("/api/users/create", user.CreateUser)
-	http.HandleFunc("/api/users/update", user.UpdateUser)
-	http.HandleFunc("/api/users/delete", user.DeleteUser)
+	http.HandleFunc("/api/users", authRequired(user.GetUsers))
+	http.HandleFunc("/api/users/get", authRequired(user.GetUser))
+	http.HandleFunc("/api/users/create", authRequired(authHandler.Register))
+	http.HandleFunc("/api/users/update", authRequired(user.UpdateUser))
+	http.HandleFunc("/api/users/delete", authRequired(user.DeleteUser))
 
 	// Group endpoints
-	http.HandleFunc("/api/groups", group.GetGroups)
-	http.HandleFunc("/api/groups/get", group.GetGroup)
-	http.HandleFunc("/api/groups/create", group.CreateGroup)
-	http.HandleFunc("/api/groups/update", group.UpdateGroup)
-	http.HandleFunc("/api/groups/delete", group.DeleteGroup)
+	http.HandleFunc("/api/groups", authRequired(group.GetGroups))
+	http.HandleFunc("/api/groups/get", authRequired(group.GetGroup))
+	http.HandleFunc("/api/groups/create", authRequired(group.CreateGroup))
+	http.HandleFunc("/api/groups/update", authRequired(group.UpdateGroup))
+	http.HandleFunc("/api/groups/delete", authRequired(group.DeleteGroup))
 
 	// Group membership endpoints
-	http.HandleFunc("/api/groups/members/add", group.AddUserToGroup)
-	http.HandleFunc("/api/groups/members/remove", group.RemoveUserFromGroup)
-	http.HandleFunc("/api/groups/members", group.GetGroupMembers)
-	http.HandleFunc("/api/users/groups", group.GetUserGroups)
+	http.HandleFunc("/api/groups/members/add", authRequired(group.AddUserToGroup))
+	http.HandleFunc("/api/groups/members/remove", authRequired(group.RemoveUserFromGroup))
+	http.HandleFunc("/api/groups/members", authRequired(group.GetGroupMembers))
+	http.HandleFunc("/api/users/groups", authRequired(group.GetUserGroups))
 
 	// Group messaging endpoints
-	http.HandleFunc("/api/groups/messages/send", group.SendGroupMessage)
+	http.HandleFunc("/api/groups/messages/send", authRequired(group.SendGroupMessage))
 
 	// DM endpoints
-	http.HandleFunc("/api/dm/groups/direct", dm.CreateDirectGroup)
-	http.HandleFunc("/api/dm/groups", dm.GetUserGroups)
-	http.HandleFunc("/api/dm/messages", dm.GetMessages)
-	http.HandleFunc("/api/dm/messages/send", dm.SendMessage)
+	http.HandleFunc("/api/dm/groups/direct", authRequired(dm.CreateDirectGroup))
+	http.HandleFunc("/api/dm/groups", authRequired(dm.GetUserGroups))
+	http.HandleFunc("/api/dm/messages", authRequired(dm.GetMessages))
+	http.HandleFunc("/api/dm/messages/send", authRequired(dm.SendMessage))
 	http.HandleFunc("/api/dm/ws", dm.HandleWebSocket)
 
 	// Institution endpoints
-	http.HandleFunc("/api/institutions", institution.GetInstitutions)
-	http.HandleFunc("/api/institutions/get", institution.GetInstitution)
-	http.HandleFunc("/api/institutions/create", institution.CreateInstitution)
-	http.HandleFunc("/api/institutions/update", institution.UpdateInstitution)
-	http.HandleFunc("/api/institutions/delete", institution.DeleteInstitution)
+	http.HandleFunc("/api/institutions", authRequired(institution.GetInstitutions))
+	http.HandleFunc("/api/institutions/get", authRequired(institution.GetInstitution))
+	http.HandleFunc("/api/institutions/create", authRequired(institution.CreateInstitution))
+	http.HandleFunc("/api/institutions/update", authRequired(institution.UpdateInstitution))
+	http.HandleFunc("/api/institutions/delete", authRequired(institution.DeleteInstitution))
 
 	// Post endpoints - use database if available
 	var postService *post.Service
@@ -170,20 +176,25 @@ func main() {
 	postHandler := post.NewHandler(postService)
 	http.HandleFunc("/api/posts", postHandler.GetPosts)
 	http.HandleFunc("/api/posts/search", postHandler.SearchPosts)
-	http.HandleFunc("/api/posts/get", postHandler.GetPost)
-	http.HandleFunc("/api/posts/create", postHandler.CreatePost)
-	http.HandleFunc("/posts/", postHandler.UpdatePost) // Updated route for UpdatePost
-	http.HandleFunc("/api/posts/delete", postHandler.DeletePost)
+	http.HandleFunc("/api/posts/get", authRequired(postHandler.GetPost))
+	http.HandleFunc("/api/posts/create", authRequired(postHandler.CreatePost))
+	http.HandleFunc("/posts/", authRequired(postHandler.UpdatePost)) // Updated route for UpdatePost
+	http.HandleFunc("/api/posts/delete", authRequired(postHandler.DeletePost))
 
 	// Comment endpoints
-	http.HandleFunc("/api/comments", comment.GetComments)
-	http.HandleFunc("/api/comments/get", comment.GetComment)
-	http.HandleFunc("/api/comments/create", comment.CreateComment)
-	http.HandleFunc("/api/comments/update", comment.UpdateComment)
-	http.HandleFunc("/api/comments/delete", comment.DeleteComment)
+	http.HandleFunc("/api/comments", authRequired(comment.GetComments))
+	http.HandleFunc("/api/comments/get", authRequired(comment.GetComment))
+	http.HandleFunc("/api/comments/create", authRequired(comment.CreateComment))
+	http.HandleFunc("/api/comments/update", authRequired(comment.UpdateComment))
+	http.HandleFunc("/api/comments/delete", authRequired(comment.DeleteComment))
 
 	// Initialize shared user service
-	userRepo := user.NewRepository()
+	var userRepo user.Repository
+	if db != nil {
+		userRepo = user.NewPostgresRepository(db)
+	} else {
+		userRepo = user.NewRepository()
+	}
 	userService := user.NewService(userRepo)
 
 	// Auth endpoints
